27470345d44fca5a2ebd4794d0e2bc7a46645639
chenzhengxi <zxchenmail@163.com>
2019-08-27 11:00:08 +0800

    new(191): the number of 1

---
+++ 191.js
@@ -0,0 +1,16 @@
+/**
+ * 编写一个函数，输入是一个无符号整数
+ * 返回其二进制表达式中数字位数为 ‘1’ 的个数（也被称为汉明重量）
+ */
+var hammingWeight = function(n) {
+  let str = n.toString(2);
+  let weight = 0;
+  for(let i=0; i<str.length; i++){
+    if(str[i]==1){
+      weight++;
+    }
+  }
+  return weight;
+};
+
+console.log(hammingWeight(0000000101000))
