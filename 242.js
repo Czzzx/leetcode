bc8bf540897b4b51d6059b643f2e195efdb4936e
chenzhengxi <zxchenmail@163.com>
2019-08-27 13:24:18 +0800

    new(242): Valid Anagram

---
+++ 242.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ * -------------------------------
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ */
+var isAnagram = function(s, t) {
+  let arrS = s.split('');
+  let arrT = t.split('');
+  arrS.sort();
+  arrT.sort();
+  return arrS.join('') === arrT.join('');
+};
+
+console.log(isAnagram("anagram","nagaram"))
