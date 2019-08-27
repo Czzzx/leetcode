6c4bef6315007dc6e02e07f8e719e4deeb4d2d92
chenzhengxi <zxchenmail@163.com>
2019-08-27 16:18:16 +0800

    new(69): Sqrtx

---
+++ 69.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} x
+ * @return {number}
+ * --------------------
+ * 实现 int sqrt(int x) 函数。
+ * 计算并返回 x 的平方根，其中 x 是非负整数。
+ * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+ */
+var mySqrt = function(x) {
+  if(x==0 || x==1) return x;
+  for(let i=1; i<x; i++){
+    if(i*i == x){
+      return i;
+    }else {
+      if(i*i < x && (i+1)*(i+1) > x){
+        return i;
+      }
+    }
+  }
+};
+
+console.log(mySqrt(8))
