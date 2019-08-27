74b25804c49db82d62c8e25b525d00155ce8fa47
chenzhengxi <zxchenmail@163.com>
2019-08-27 15:51:58 +0800

    new(125): Valid Palindrome

---
+++ 125.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ * ----------------------
+ * 给定一个字符串，验证它是否是回文串
+ * 只考虑字母和数字字符，可以忽略字母的大小写。
+ */
+var isPalindrome = function(s) {
+  if(s.length<=0) return true;
+  s = s.toLowerCase();
+  let left = 0, right = s.length-1;
+  while(left < right){
+    if(s[left].match(/[0-9a-zA-Z]/) && s[right].match(/[0-9a-zA-Z]/)){
+      if(s[left] != s[right]) return false;
+      left++;
+      right--;
+    }else if(!s[left].match(/[0-9a-zA-Z]/)){
+      left++;
+    }else{
+      right--;
+    }
+  }
+  return true;
+};
+
+console.log(isPalindrome("0P"))
