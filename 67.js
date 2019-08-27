76c00f1936995b584445b0078e64a8e529835b4b
chenzhengxi <zxchenmail@163.com>
2019-08-27 15:24:35 +0800

    new(67): Add Binary

---
+++ 67.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ * ---------------------------
+ * 给定两个二进制字符串，返回他们的和（用二进制表示）。
+ * 输入为非空字符串且只包含数字 1 和 0
+ */
+var addBinary = function(a, b) {
+  let aIndex = a.length-1;
+  let bIndex = b.length-1;
+  if(aIndex >= bIndex){
+    for(let i=0; i<aIndex-bIndex; i++){
+      b = '0' + b;
+    }
+  } else {
+    for(let i=0; i<bIndex-aIndex; i++){
+      a = '0' + a;
+    }
+  }
+  let carry = 0;
+  let ret = '';
+  for(let i=a.length-1; i>=0; i--){
+    if(a[i] == '1' && b[i] == '1'){
+      ret = carry + ret;
+      carry = 1;
+    }else if(a[i] == '0' && b[i] == '0'){
+      ret = carry + ret;
+      carry = 0;
+    }else {
+      if(carry == 0){
+        ret = 1 + ret;
+      } else {
+        ret = 0 + ret;
+      }
+    }
+  }
+  if(carry==1){
+    ret = 1 + ret;
+  }
+  return ret;
+};
+
+console.log(addBinary("1010", "1011"))
