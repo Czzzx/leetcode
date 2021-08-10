deb7f44c98350856505af36b6568e333370e067c
zhengxichen <zhengxichen@tencent.com>
2021-08-10 17:36:43 +0800

    dp 3

---
+++ 1-100/70.js
@@ -0,0 +1,26 @@
+/**
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ */
+
+ /**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+
+    let a = 1;
+    let b = 2;
+
+    for(let i = 3; i <= n; i++) {
+        let temp = a + b;
+
+        a = b;
+        b = temp;
+    }
+
+    return b;
+};
